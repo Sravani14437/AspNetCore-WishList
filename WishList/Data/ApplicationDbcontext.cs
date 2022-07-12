96b4196f9f52c201cb0c8912b4b0101cc314545f
Potluri Sravani <M1082278@mindtree.com>
2022-07-12 17:54:03 +0530

    changes

--- WishList/Data/ApplicationDbcontext.cs
+++ WishList/Data/ApplicationDbcontext.cs
@@ -1,14 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
-using WishList.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
-
-        public DbSet<Models.Item> Items { get; set; }
     }
+}
